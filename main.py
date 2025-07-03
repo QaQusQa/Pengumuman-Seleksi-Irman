fb5cba7f812c13cba331b1885c2b683866f59ea0
Qaqa Qushayyi Qatrunnada <qaqaqushayyiq@gmail.com>
2025-07-04 00:53:57 +0700

    second commit

---
+++ main.py
@@ -0,0 +1,5 @@
+
+for i in range (73358763, 73358811 ) :
+    print(f"00{i}")
+    a = len(i)
+    print(a)
--- script.js
+++ script.js
@@ -1,27 +1,54 @@
 
 // Sekedar simulasi
 const data = [
-    { nisn: "123", tanggal: "01-01-2007", nama: "Ckckckck", status: "LOLOS" },
-    { nisn: "111", tanggal: "02-02-2007", nama: "Hahahah", status: "LOLOS" },
-    { nisn: "222", tanggal: "03-03-2009", nama: "Heheheheh", status: "TIDAK LOLOS" },
-    { nisn: "321", tanggal: "04-04-2010", nama: "Wkwkwkw", status: "TIDAK LOLOS"}
-    
-    
-    
-    
+    { nisn: "0073358763", tanggal: "25-09-2006", nama: "Abidah Ramadani", status: "LOLOS" },
+    { nisn: "0073358764", tanggal: "06-12-2006", nama: "Affan Ikhsani An Naba", status: "LOLOS" },
+    { nisn: "0073358765", tanggal: "17-07-2008", nama: "Aira Alerta Berliana", status: "TIDAK LOLOS" },
+    { nisn: "0073358766", tanggal: "04-04-2010", nama: "Aisyah Faqih Hatul Ulum", status: "TIDAK LOLOS" },
+    { nisn: "0073358767", tanggal: "05-12-2010", nama: "Akila Putri Alfiani", status: "TIDAK LOLOS" },
     // bisa ditambahkan data lagi
 ];
 
-function cekHasil() {
+function autoNext(input, nextId) {
+    if (input.value.length >= 10) {
+        document.getElementById(nextId).focus(); //setelah digit NIR terpenuhi, langsung enter ke tanggal lahir
+    }
+}
+
+function cekEnter(event, nextId) { //setelah NIR dan tanggal terpenuhi, bisa cek hasil dengan tekan enter
+    if (event.key === "Enter") {
+        event.preventDefault();
+        const currentInput = event.target.value.trim();
+        if (currentInput !== "") {
+            if (nextId === "submit") {
+                cekHasil();
+            } else {
+                document.getElementById(nextId).focus();
+            }
+        }
+    
+    }
+}
+
+function formatDateToID(dateStr) { // fortmat kalender 
+    const date = new Date(dateStr);
+    const dd = String(date.getDate()).padStart(2, '0');
+    const mm = String(date.getMonth() + 1).padStart(2, '0');
+    const yyyy = date.getFullYear();
+    return `${dd}-${mm}-${yyyy}`;
+}
+
+function cekHasil() { //fungsi cek hasil menggunakan ID di HTML
     const nisn = document.getElementById("nisn").value.trim();
-    const tanggal = document.getElementById("tanggal").value.trim();
+    const tanggalRaw = document.getElementById("tanggal").value;
+    const tanggal = formatDateToID(tanggalRaw);
     const hasilDiv = document.getElementById("hasil");
 
     const peserta = data.find(item=> item.nisn === nisn && item.tanggal === tanggal);
 
     if (peserta) {
-        const ucapan = peserta.status === "LOLOS" ? 
-        "<br><br> SELAMAT semoga dapat menjalankan tugas dengan amanah dan tanggung jawab." :
+        const ucapan = peserta.status === "LOLOS" ? // ? "True" : "False"
+        "<br><br> SELAMAT! semoga dapat menjalankan tugas dengan amanah dan tanggung jawab." :
         "<br><br> tetap SEMANGAT dan jangan putus asa.";
 
         hasilDiv.innerHTML = `${peserta.nama} dinyatakan <br><br>${peserta.status}${ucapan}`;
